b6224cb7233546008347abc3d810ee58bf1fe600
MisterL33 <laurent.julienne@ynov.com>
2017-11-23 15:27:35 +0100

    marche

--- test/server.test.js
+++ test/server.test.js
@@ -1,26 +1,27 @@
 require('mocha')
 const request = require('supertest')
 const server = require('../server/server')
+require('chai').should()
 
-xdescribe('Routes', () => {
+describe('Routes', () => {
     it('should pass index', () => {
             return request(server).get('/index').then((res) => {
-            res.status.should.be.equal(200)
+            res.status.should.equal(200)
         })
     })
 
     xit('should pass create', () => {
         return request(server).get('/create').then((res) => {
-            res.status.should.be.equal(200)
-            res.data.should.not.be.equal(null)
-            res.data.should.not.be.equal('object')
+            res.status.should.equal(200)
+            res.data.should.not.equal(null)
+            res.data.should.not.equal('object')
         })
     })
 
     xit('should pass delete', () => {
         return request(server).get('/delete/:id').then((res) => {
-            res.status.should.be.equal(200)
-            res.params.should.be.equal(res.params.uid)
+            res.status.should.equal(200)
+            res.params.should.equal(res.params.uid)
         })
     })
 })
